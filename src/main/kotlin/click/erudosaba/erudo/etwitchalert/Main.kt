07ff8e8e02d1fd80cec1ee07cf0c18eaa4612247
erudo <hashierudo@gmail.com>
2020-12-25 19:31:20 +0900

    first commit

---
+++ src/main/kotlin/click/erudosaba/erudo/etwitchalert/Main.kt
@@ -0,0 +1,14 @@
+package click.erudosaba.erudo.etwitchalert
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Main : JavaPlugin() {
+
+    override fun onDisable() {
+        super.onDisable()
+    }
+
+    override fun onEnable() {
+        super.onEnable()
+    }
+}
